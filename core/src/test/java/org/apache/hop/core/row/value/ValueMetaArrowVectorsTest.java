13061513a3c6ed0d4c272b09e135187d83be7e81
Dave Voutila <voutilad@gmail.com>
2022-08-09 16:48:19 -0400

    apply google java formatting

--- core/src/test/java/org/apache/hop/core/row/value/ValueMetaArrowVectorsTest.java
+++ core/src/test/java/org/apache/hop/core/row/value/ValueMetaArrowVectorsTest.java
@@ -1,5 +1,7 @@
 package org.apache.hop.core.row.value;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.IntVector;
@@ -11,47 +13,44 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class ValueMetaArrowVectorsTest {
-    @BeforeClass
-    public static void before() throws Exception {
-        HopClientEnvironment.init();
-    }
+  @BeforeClass
+  public static void before() throws Exception {
+    HopClientEnvironment.init();
+  }
 
-    @Test
-    public void testCloneData() throws HopValueException {
-        BufferAllocator allocator = ArrowBufferAllocator.rootAllocator();
-        FieldVector[] vectors = new FieldVector[] {
-            new IntVector("a", allocator), new IntVector("b", allocator),
+  @Test
+  public void testCloneData() throws HopValueException {
+    BufferAllocator allocator = ArrowBufferAllocator.rootAllocator();
+    FieldVector[] vectors =
+        new FieldVector[] {
+          new IntVector("a", allocator), new IntVector("b", allocator),
         };
 
-        for (FieldVector valueVector : vectors) {
-            valueVector.allocateNewSafe();
-            ((IntVector) valueVector).set(0, 42);
-            valueVector.setValueCount(1);
-        }
-
-        ValueMetaArrowVectors valueMeta = new ValueMetaArrowVectors(
-                "test",
-                new Schema(Arrays
-                        .stream(vectors)
-                        .map(FieldVector::getField)
-                        .collect(Collectors.toList())));
-
-        FieldVector[] cloned = (FieldVector[]) valueMeta.cloneValueData(vectors);
-        Assert.assertNotSame(vectors, cloned);
-        Assert.assertEquals("a", cloned[0].getName());
-        Assert.assertEquals("b", cloned[1].getName());
-
-        for (FieldVector vector : vectors) {
-            Assert.assertEquals(42, vector.getObject(0));
-            vector.close();
-        }
-        for (FieldVector vector : cloned) {
-            Assert.assertEquals(42, vector.getObject(0));
-            vector.close();
-        }
+    for (FieldVector valueVector : vectors) {
+      valueVector.allocateNewSafe();
+      ((IntVector) valueVector).set(0, 42);
+      valueVector.setValueCount(1);
     }
+
+    ValueMetaArrowVectors valueMeta =
+        new ValueMetaArrowVectors(
+            "test",
+            new Schema(
+                Arrays.stream(vectors).map(FieldVector::getField).collect(Collectors.toList())));
+
+    FieldVector[] cloned = (FieldVector[]) valueMeta.cloneValueData(vectors);
+    Assert.assertNotSame(vectors, cloned);
+    Assert.assertEquals("a", cloned[0].getName());
+    Assert.assertEquals("b", cloned[1].getName());
+
+    for (FieldVector vector : vectors) {
+      Assert.assertEquals(42, vector.getObject(0));
+      vector.close();
+    }
+    for (FieldVector vector : cloned) {
+      Assert.assertEquals(42, vector.getObject(0));
+      vector.close();
+    }
+  }
 }
